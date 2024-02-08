2b407ebf9703f14abcdf5ea59c40790bf4eb8331
Rodrigo Martínez <r.0.martinez.agustin@gmail.com>
2024-02-08 05:20:21 -0600

    Se agrega lógica básica del encriptador/desencriptador

--- cryptLogic.js
+++ cryptLogic.js
@@ -1,16 +1,47 @@
-function getText(){
+var lock = true;
+function getText(){ 
     return document.getElementById("textToCrypt").value;
 }
-
-function crypting(){
-    if (typeof getText() != "undefined"){
-        document.getElementById("");
-        console.log(getText()); 
+function startProcess(){
+    if(getText().length === 1){
+        clearText();
+    }
+    if (getText() != ""){
+        setText(getText());
     }
     else{
-        notFound.style.display = "initial";
+        clearText();
+        document.getElementById("result").innerHTML ="Mensaje no encontrado";
     }
     /* Verificar cuando se borra un caracter */
-
 }
+function setText(string){
+    if (lock) {
+        document.getElementById("result").innerHTML = string.replaceAll("e","enter")
+                                                        .replaceAll("i","imes")
+                                                        .replaceAll("a","ai")
+                                                        .replaceAll("o","ober")
+                                                        .replaceAll("u","ufat");
+    }
+    else{
+        document.getElementById("result").innerHTML = string.replaceAll("imes","i")
+                                                        .replaceAll("enter","e")
+                                                        .replaceAll("ai","a")
+                                                        .replaceAll("ober","o")
+                                                        .replaceAll("ufat","u");
+    }
+    
+    
+    
+}
+function verifyMode(){
+
+    document.getElementById("textToCrypt").value = "";
+    document.getElementById("result").innerHTML ="Mensaje no encontrado";
+    return lock = !lock;
+}
+function clearText(){
+    document.getElementById("result").innerHTML = "";
+}
+
 
