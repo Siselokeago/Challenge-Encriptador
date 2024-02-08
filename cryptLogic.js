83c5cb10d2466c1fa41fc48f9c04e7e4bb0d6489
Rodrigo Martínez <r.0.martinez.agustin@gmail.com>
2024-02-07 21:25:29 -0600

    se agregaron animaciones al introducir texto y cambio de color

---
+++ cryptLogic.js
@@ -0,0 +1,16 @@
+function getText(){
+    return document.getElementById("textToCrypt").value;
+}
+
+function crypting(){
+    if (typeof getText() != "undefined"){
+        document.getElementById("");
+        console.log(getText()); 
+    }
+    else{
+        notFound.style.display = "initial";
+    }
+    /* Verificar cuando se borra un caracter */
+
+}
+
