0989698053b62e16a6eb330a42e5a96a8c76a636
Rodrigo Martínez <r.0.martinez.agustin@gmail.com>
2024-02-26 03:10:09 -0600

    Se creó la versión del escriptador con el diseño de alura, se agregó los botones correspondientes para el cambio de pagina, logica mejorada de encriptador

--- aluraDesign/aluralogic.js
+++ aluraDesign/aluralogic.js
@@ -1,7 +1,7 @@
 var lock = true;
-var notValid = false;
 var getText = document.getElementById("textToCrypt");
 var placeText = document.getElementById("result");
+const initialHiddenBoxes = document.querySelectorAll('.initialHide');
 const claves = {
     a: "ai",
     e: "enter",
@@ -13,10 +13,16 @@ const claves = {
 var clavesExp = [RegExp(Object.keys(claves).join('|'), 'g'),
 RegExp(Object.values(claves).join('|'), 'g')];
 
+function valuingArea(topbot, center){
+    initialHiddenBoxes.forEach(element => {
+        element.style.display = topbot;
+    });
+    document.getElementById("center-container").style.display = center;
+}
 function startProcess() {
+    valuingArea("flex" , "none");
     if (getText.value === "") {
-        placeText.textContent = "Mensaje no encontrado";
-        validate();
+        valuingArea("none" , "flex");
     }
     else {
         setText(getText.value);
@@ -39,27 +45,15 @@ function setText(string) {
 function cleanText(str) {
     var stringClean = str
         .replace(/[^A-Za-z0-9 .,\u00f1]/g, function (match) {
-            notValid = true;
             return '';
         })
         .replace(/[A-Z]/g, function (match) {
-            notValid = true;
             return match.toLowerCase();
         });
-    validate();
     return stringClean;
 }
-function switchMode() {
-    validate();
-    getText.value = "";
-    placeText.textContent = "Mensaje no encontrado";
-    lock = !lock;
-}
 function copyResult() {
+    valuingArea("none" , "flex");
+    getText.value = "";
     navigator.clipboard.writeText(placeText.textContent)
-}
-function validate() {
-    document.getElementById("error").textContent = notValid === false ? "" : "Error: Mayúsculas y/o carácteres especiales detectados y corregidos";
-    document.getElementById("hideError").checked = notValid;
-    notValid = false;
 }
